793455ff6315cb4b1db95d69d9a70ca9701f01c1
smile_00112 <ooo.smile2000@yandex.ru>
2025-03-08 17:17:24 +0300

    test commit

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -14,6 +14,11 @@ const mainNavItems: NavItem[] = [
         href: '/dashboard',
         icon: LayoutGrid,
     },
+    {
+        title: 'Products',
+        href: '/products',
+        icon: LayoutGrid,
+    },
 ];
 
 const footerNavItems: NavItem[] = [
